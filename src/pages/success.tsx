6e76c2f4e38c2ce1e3fd29dbf8ed9d8e7ee0c5ae
Rouak-stha <rounakshrestha1153@gmail.com>
2023-04-19 19:25:08 +0545

    Maintain Layout

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,3 @@
 export default function Success() {
-	return <h1 className='text-xl font-bold'>Your OTP is verified</h1>
+	return <h1 className='text-xl font-bold mb-40'>Your OTP is verified</h1>
 }
